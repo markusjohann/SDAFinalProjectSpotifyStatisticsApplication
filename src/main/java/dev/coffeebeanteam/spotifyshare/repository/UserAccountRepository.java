725c73d6e3554726d25b5f84afd9f23379a09c6a
pj <Prt Johanson>
2023-08-23 18:45:25 +0300

    Remove unsed import.

--- src/main/java/dev/coffeebeanteam/spotifyshare/repository/UserAccountRepository.java
+++ src/main/java/dev/coffeebeanteam/spotifyshare/repository/UserAccountRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
