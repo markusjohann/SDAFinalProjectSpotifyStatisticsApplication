da188a45c302b9b89c06e943e389af8bc0c96d64
pj <Prt Johanson>
2023-08-23 18:44:14 +0300

    Delete unused files.

--- src/main/java/dev/coffeebeanteam/spotifyshare/repository/SpotifyAuthorizedClientRepository.java
+++
@@ -1,8 +0,0 @@
-package dev.coffeebeanteam.spotifyshare.repository;
-
-import dev.coffeebeanteam.spotifyshare.model.AuthorizedClientEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SpotifyAuthorizedClientRepository extends JpaRepository<AuthorizedClientEntity, String> {
-    AuthorizedClientEntity findByClientRegistrationIdAndPrincipalName(String clientRegistrationId, String principalName);
-}
