7821e9420cf18cf63f8e57b561758d1b470b3d3c
pj <Prt Johanson>
2023-08-27 09:34:55 +0300

    Remove some unused imports.

--- src/main/java/dev/coffeebeanteam/spotifyshare/service/SharingService.java
+++ src/main/java/dev/coffeebeanteam/spotifyshare/service/SharingService.java
@@ -6,9 +6,7 @@ import dev.coffeebeanteam.spotifyshare.model.UserAccount;
 import dev.coffeebeanteam.spotifyshare.model.UserAccountSharing;
 import dev.coffeebeanteam.spotifyshare.model.UserAccountSharingKey;
 import dev.coffeebeanteam.spotifyshare.repository.UserAccountSharingRepository;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
