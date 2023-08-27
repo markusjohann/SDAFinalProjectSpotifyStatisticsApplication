d3b7e99b158f8b75a3e0243739a4db85dc5b2305
pj <Prt Johanson>
2023-08-27 08:25:09 +0300

    Create root page index controller and redirect it to dashboard.

--- src/main/java/dev/coffeebeanteam/spotifyshare/controller/IndexController.java
+++ src/main/java/dev/coffeebeanteam/spotifyshare/controller/IndexController.java
@@ -1,2 +1,14 @@
-package dev.coffeebeanteam.spotifyshare.controller;public class IndexController {
+package dev.coffeebeanteam.spotifyshare.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+    @GetMapping
+    public String index() {
+        return "redirect:/dashboard";
+    }
 }
